bdd97e426a1e1285be6552a1e17284d8a48ca7a0
zsmerritt <zsmerritt@gmail.com>
2019-04-07 13:46:17 -0700

    more bug testing

--- makeNN.py
+++ makeNN.py
@@ -169,7 +169,7 @@ def trainAndSaveGenerator(model,epochs,name,target_size,batch_size,model_save_fi
 			workers=6,
 			callbacks=[
 				EarlyStopping(patience=8, restore_best_weights=True),
-				ReduceLROnPlateau(patience=3,factor=0.7,min_lr=0.001)
+				ReduceLROnPlateau(patience=3,factor=0.7,min_lr=0.001),
 				ModelCheckpoint(model_save_filepath, monitor='val_loss', save_best_only=True)
 
 			])
