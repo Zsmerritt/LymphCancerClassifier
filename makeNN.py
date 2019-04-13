7c9db50d1c56fac30578e38ff88ac2a20f927c6b
zsmerritt <zsmerritt@gmail.com>
2019-04-12 17:02:12 -0700

    fixing bad syntax

--- makeNN.py
+++ makeNN.py
@@ -71,7 +71,7 @@ def train_generator_with_batch_schedule(
 						model_save_filepath):
 
 	epochs=epochs//3
-	max_queue_size=[32:10:3]
+	max_queue_size=[32,10,3]
 
 	train_gen = DataGenerator(
 		data_folder=trainSetFolder,
