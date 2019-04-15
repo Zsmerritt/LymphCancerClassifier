f65520079471cb3d2e9b3570be02e35f332f1a05
zsmerritt <zsmerritt@gmail.com>
2019-04-14 19:02:34 -0700

    adjusted max q size

--- makeNN.py
+++ makeNN.py
@@ -77,7 +77,7 @@ def train_generator_with_batch_schedule(
 						model_save_filepath):
 
 	epochs=epochs//3
-	max_queue_size=[40,20,5]
+	max_queue_size=[20,10,5]
 
 	train_gen = DataGenerator(
 		data_folder=trainSetFolder,
